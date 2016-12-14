1afa453c4967a080f7694eb0c6a019751ff95d30
Espyfudge <rutger101_@hotmail.nl>
2016-12-14 10:15:03 +0100

    Add files via upload

---
+++ Source/Main.hx
@@ -0,0 +1,26 @@
+package;
+
+import openfl.Assets;
+import openfl.display.Sprite;
+import openfl.events.KeyboardEvent;
+import openfl.display.Bitmap;
+
+
+class Main extends Sprite {
+
+	var player : Player;
+	
+
+  	public function new () {
+		
+		super ();
+
+		player = new Player();
+		addChild( player );
+		
+		stage.addEventListener(KeyboardEvent.KEY_DOWN, player.keyDown );
+		stage.addEventListener(KeyboardEvent.KEY_UP, player.keyUp );
+		
+	}
+
+}
