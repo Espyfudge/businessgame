52c037ae6655fb9db45a181b56e681113842de66
Espyfudge <rutger101_@hotmail.nl>
2016-12-15 17:27:36 +0100

    Add files via upload

--- Source/Main.hx
+++ Source/Main.hx
@@ -4,12 +4,12 @@ import openfl.Assets;
 import openfl.display.Sprite;
 import openfl.events.KeyboardEvent;
 import openfl.display.Bitmap;
-
+import openfl.Lib;
+import openfl.events.Event;
 
 class Main extends Sprite {
 
 	var player : Player;
-	
 
   	public function new () {
 		
@@ -21,6 +21,10 @@ class Main extends Sprite {
 		stage.addEventListener(KeyboardEvent.KEY_DOWN, player.keyDown );
 		stage.addEventListener(KeyboardEvent.KEY_UP, player.keyUp );
 		
+		
 	}
+
+	
+
 
 }
